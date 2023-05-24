5e93e2f7efbaf42c06eb611212fff4d9a24a7de9
Masterchef842 <robertwilliamlord55@gmail.com>
2023-05-24 00:35:48 -0500

    all routes working

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -17,14 +17,14 @@ foreignKey: {name: 'category_id'}
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag,{
   through: {model: ProductTag},
-  foreignKey: {name: 'product_id'}
+  //foreignKey: {name: 'product_id'}
 })
 
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product,{
   through: {model: ProductTag},
-  foreignKey: {name: 'tag_id'}
+  //foreignKey: {name: 'tag_id'}
 })
 
 module.exports = {
--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -4,18 +4,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async (req, res) => {
-  // find all products
-  try{
-    const productData= await Product.findAll({
-      include: [{model: Tag, through: ProductTag, as : "product_tags"}]
+router.get("/", (req, res) => {
+  Product.findAll({
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag,
+      },
+    ],
+  })
+    .then((products) => res.json(products))
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
     });
-
-    res.status(200).json(productData)
-  }catch(err){
-    res.status(500).json(err);
-  }
-  // be sure to include its associated Category and Tag data
 });
 
 // get one product
@@ -23,7 +26,13 @@ router.get('/:id',async (req, res) => {
   // find a single product by its `id`
   try{
     const productData= await Product.findByPk(req.params.id,{
-      include: [{model: Tag, through: ProductTag, as : "product_tags"}]
+      include: [
+        Category,
+        {
+          model: Tag,
+          through: ProductTag,
+        },
+      ],
     });
     if(! productData){
       res.status(404).json({message: 'No product with this ID'});
