43d380dacf797d80bd88b5193592c6bd696373fc
Masterchef842 <robertwilliamlord55@gmail.com>
2023-05-23 21:49:49 -0500

    added routes, untested

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,21 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: category_id
+  foreignKey: {name: 'category_id'}
 })
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: category_id
+foreignKey: {name: 'category_id'}
 })
 
 
 // Products belongToMany Tags (through ProductTag)
-ProductTag.belongsTo(Tag,{
-  foreignKey: tag_id
+Product.belongsToMany(Tag,{
+  through: {model: ProductTag},
+  foreignKey: {name: 'product_id'}
 })
 
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product,{
+  through: {model: ProductTag},
+  foreignKey: {name: 'tag_id'}
+})
 
 module.exports = {
   Product,
